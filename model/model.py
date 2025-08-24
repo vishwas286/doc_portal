3309ac3ac43096a11755b1bc95837dbe6aa04b9a
vishwas <vishwassoni1990@example.com>
2025-08-24 21:17:40 +0530

    document comparison structure is ready

--- model/model.py
+++ model/model.py
@@ -1,1 +1,21 @@
-from typing import List, Union
+from pydantic import BaseModel, RootModel ,Field
+from typing import Optional, List, Dict, Any, Union
+
+
+class Metadata(BaseModel):
+    Summary: List[str] = Field(default_factory=list, description="Summary of the document")
+    Title: str
+    Author: str
+    DateCreated: str   
+    LastModifiedDate: str
+    Publisher: str
+    Language: str
+    PageCount: Union[int, str]  # Can be "Not Available"
+    SentimentTone: str
+
+class ChangeFormat(BaseModel):
+    Page: str
+    changes: str
+    
+class SummaryResponse(RootModel[list[ChangeFormat]]):
+    pass
---
+++ src/document_comprison/document_comparator.py
@@ -0,0 +1,42 @@
+import sys
+from dotenv import load_dotenv
+import pandas as pd
+from logger.custom_logger import CustomLogger
+from exception.custom_exception import DocumentPortalException
+from model.model import *
+from prompt.promt_pre import PROMPT_REGISTRY
+from utils.model_loader import ModelLoader
+from langchain_core.output_parsers import JsonOutputParser
+from langchain.output_parsers import OutputFixingParser
+
+class DocumentComparatorLLM:
+    def __init__(self):
+        load_dotenv()
+        self.log = CustomLogger().get_logger(__name__)
+        self.loader = ModelLoader()
+        self.llm = self.loader.load_llm()
+        self.parser = JsonOutputParser(pydantic_object=SummaryResponse)
+        self.fixing_parser = OutputFixingParser.from_llm(parser=self.parser, llm=self.llm)
+        self.prompt = PROMPT_REGISTRY["document_comparison"]
+        self.chain = self.prompt | self.llm | self.parser | self.fixing_parser
+        self.log.info("DocumentComparatorLLM initialized with model and parser.")
+    
+    def compare_documents(self):
+        """
+        Compares two documents and returns a structured comparison.
+        """
+        try:
+            pass
+        except Exception as e:
+            self.log.error(f"Error in compare_documents: {e}")
+            raise DocumentPortalException("An error occurred while comparing documents.",sys)
+    
+    def _format_response(self):
+        """
+        Formats the response from the LLM into a structured format.
+        """
+        try:
+            pass
+        except Exception as e:
+            self.log.error("Error formatting response into DataFrame", error=str(e))
+            raise DocumentPortalException("Error formatting response",sys)
