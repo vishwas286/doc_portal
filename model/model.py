efac9fb7b8daee0db390acdac100355fa887a238
vishwas <vishwassoni1990@example.com>
2025-08-27 15:45:49 +0530

    doc comapison done

--- model/model.py
+++ model/model.py
@@ -1,12 +1,12 @@
-from pydantic import BaseModel, RootModel ,Field
-from typing import Optional, List, Dict, Any, Union
-
+from pydantic import BaseModel, RootModel
+from typing import List, Union
+from enum import Enum
 
 class Metadata(BaseModel):
-    Summary: List[str] = Field(default_factory=list, description="Summary of the document")
+    Summary: List[str]
     Title: str
-    Author: str
-    DateCreated: str   
+    Author: List[str]
+    DateCreated: str
     LastModifiedDate: str
     Publisher: str
     Language: str
@@ -15,7 +15,13 @@ class Metadata(BaseModel):
 
 class ChangeFormat(BaseModel):
     Page: str
-    changes: str
-    
+    Changes: str
+
 class SummaryResponse(RootModel[list[ChangeFormat]]):
     pass
+
+class PromptType(str, Enum):
+    DOCUMENT_ANALYSIS = "document_analysis"
+    DOCUMENT_COMPARISON = "document_comparison"
+    CONTEXTUALIZE_QUESTION = "contextualize_question"
+    CONTEXT_QA = "context_qa"
